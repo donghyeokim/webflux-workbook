a0d57cc012a39d1e953e9d858af176d8f1df022e
donghyeok.im <donghyeok.im@nhnpayco.com>
2025-01-01 23:18:45 +0900

    commit 누락

--- src/test/java/dev/practice/webfluxworkbook/ch13_testing/GeneralTestExampleTest.java
+++ src/test/java/dev/practice/webfluxworkbook/ch13_testing/GeneralTestExampleTest.java
@@ -1,4 +1,50 @@
+package dev.practice.webfluxworkbook.ch13_testing;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.StepVerifierOptions;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GeneralTestExampleTest {
-  
+
+    @Test
+    public void sayHelloTest() {
+        StepVerifier
+                .create(GeneralTestExample.sayHello())
+                .expectSubscription()
+                .as("# expect subscription")
+                .expectNext("Hi")
+                .as("# expect Hi")
+                .expectNext("Reactor")
+                .as("# expect Reactor")
+                .verifyComplete();
+    }
+
+    @Test
+    public void divideByTwoTest() {
+        Flux<Integer> source = Flux.just(2, 4, 6, 8, 10);
+        StepVerifier
+                .create(GeneralTestExample.divideByTwo(source))
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    public void takeNumberTest() {
+        Flux<Integer> source = Flux.range(0, 1000);
+        StepVerifier
+                .create(GeneralTestExample.taskNumber(source, 500),
+                        StepVerifierOptions.create().scenarioName("Verify from 0 to 499"))
+                .expectSubscription()
+                .expectNext(0)
+                .expectNextCount(498)
+                .expectNext(500)
+                .expectComplete()
+                .verify();
+    }
+
 }
